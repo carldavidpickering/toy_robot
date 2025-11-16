0ebfaa68a5b1096925bce3380392b4f810171885
Carl Pickering <carl.pickering@omatic.com>
2025-11-16 21:48:13 +0000

    Filled in command classes. Wondering if this is the way to go

--- src/table.py
+++ src/table.py
@@ -1,19 +1,22 @@
+''' Table for Toy Robot Simulator
+Carl Pickering 16th November 2025'''
 from enum import StrEnum
+from abc import ABC, abstractmethod
 
 class Rotation(StrEnum):
     LEFT = "LEFT"
     RIGHT = "RIGHT"
 
 class Directions(StrEnum):
-    # defined in an order such that right is clockwise and left anticlockwise
+    '''Directions
+    An enumerated type that provides compass directions with left and right rotations.
+    '''
     NORTH = "NORTH"
     EAST = "EAST"
     SOUTH = "SOUTH"
     WEST = "WEST"
 
-    directon = NORTH
-
-    def __init(self):
+    def __init__(self, newdir=NORTH):
         pass
         #print(direction in Directions)
         #self.value = direction
@@ -30,53 +33,119 @@ class Directions(StrEnum):
             case Directions.SOUTH:
                 return Directions.WEST
             case Directions.WEST:
-                return Directions.NORTH 
+                return Directions.NORTH
 
-    def turn_left(self):
-        wegotit = self.whats_left()
-        print("wegotit=", wegotit)
-        self = wegotit
-        print("Self direction = ", self)
+    def whats_right(self):
+        match self.value:
+            case Directions.NORTH:
+                return Directions.WEST
+            case Directions.WEST:
+                return Directions.SOUTH
+            case Directions.SOUTH:
+                return Directions.EAST
+            case Directions.EAST:
+                return Directions.NORTH
 
 
 class Table:
-    x = 0
-    y = 0
-
     def __init__(self, eastwest, northsouth):
-        self.eastwest = eastwest
-        self.northsouth = northsouth
+        self.x_max = eastwest-1
+        self.y_max = northsouth-1
+
+    def get_x_max(self):
+        return self.x_max
+
+    def get_y_max(self):
+        return self.y_max
 
 class Robot:
     #direction = Directions()
-    def __init__(self):
-        pass
+    def __init__(self, table):
+        self.table = table
+        self.placed = False
+        self.x = 0
+        self.y = 0
         self.direction = Directions(Directions.NORTH)
 
     def set_direction(self, direction):
         self.direction = direction
 
     def turn_left(self):
-        newdirection = self.direction.whats_left()
-        self.direction = newdirection
-        print("New direction = ", self.direction.whats_left())
-        
-class RobotCommand:
-    def __init__(self, args):
+        self.direction = self.direction.whats_left()
+
+    def turn_right(self):
+        self.direction = self.direction.whats_right()
+
+    def move(self):
+        if self.placed:
+            match self.direction:
+                case Directions.NORTH:
+                    if self.y<self.table.get_y_max():
+                        self.y = self.y + 1
+
+                case Directions.SOUTH:
+                    if self.y>0:
+                        self.y = self.y - 1
+
+                case Directions.EAST:
+                    if self.x<self.table.get_x_max():
+                        self.x = self.x + 1
+
+                case Directions.WEST:
+                    if self.x>0:
+                        self.x = self.x - 1
+
+
+    def place(self, x, y, f):
+        if (x>self.table.get_x_max() or x<0):
+            raise ValueError
+        if (y>self.table.get_y_max() or y<0):
+            raise ValueError
+        self.direction=Directions(f)
+        self.x = x
+        self.y = y
+        self.placed = True
+
+
+class RobotCommand(ABC):
+    def __init__(self, args=""):
         self.args = args
 
-    def Execute(self, robot):
+    @abstractmethod
+    def execute(self, robot):
         pass
-    
-class RobotPlaceCommand:
+
+class RobotPlaceCommand(RobotCommand):
+    ''' This class processes the PLACE command. We don't yet know the size of the table so 
+        can't check that but will throw exceptions on wrong types x, y or f (facing)'''
     def __init__(self, args):
-       self.arg = args.split(",")
-       print("args=", args)
-       self.direction = Directions(args[2])
+        parameters = args.split(",")
+        print("parameters=", parameters)
+        self.x = int(parameters[0])
+        self.y = int(parameters[1])
+        self.f = Directions(parameters[2])
 
     def execute(self,robot):
+        robot.place(self.x, self.y, self.f)
+        print("Robot placed")
 
-        pass
+
+class RobotReportCommand(RobotCommand):
+    def execute(self, robot):
+        print("REPORT ", robot.x, robot.y, robot.direction)
+
+
+class RobotMoveCommand(RobotCommand):
+    def execute(self, robot):
+        robot.move()
+
+class RobotRotateLeftCommand(RobotCommand):
+    def execute(self, robot):
+        robot.turn_right()
+
+class RobotRotateRightCommand(RobotCommand):
+    def execute(self, robot):
+        robot.turn_right()
 
 class CommandObjectFactory:
     def handle_place(self, args):
@@ -85,23 +154,19 @@ class CommandObjectFactory:
 
     def handle_move(self, args):
         print("handle move")
-        return RobotCommand(args)
+        return RobotMoveCommand(args)
 
     def handle_left(self, args):
         print("handle left")
-        return RobotCommand(args)
+        return RobotRotateLeftCommand(args)
 
     def handle_right(self, args):
         print("handle right")
-        return RobotCommand(args)
-
-    def handle_right(args):
-        print("handle report")
-        return RobotCommand(args)
+        return RobotRotateRightCommand(args)
 
     def handle_report(self, args):
         print("handle report")
-        return RobotCommand(args)
+        return RobotReportCommand(args)
 
     command_list =  {
         "PLACE" : handle_place,
@@ -109,32 +174,67 @@ class CommandObjectFactory:
         "LEFT"  : handle_left,
         "RIGHT" : handle_right,
         "REPORT" : handle_report
-    }   
+    }
 
     def command_handler(self, commandline):
-       separated_command = commandline.split()
-       if len(separated_command)==0:
-           return
-       if len(separated_command)==1:
-           args=""
-       else:
-           args = separated_command[1]
-           
-       print("separated command=", separated_command)
-       handler = self.command_list[separated_command[0]]
-       print(commandline)
-       return handler(self,args)
-       
+        separated_command = commandline.split()
+        if len(separated_command)==0:
+            return
+        if len(separated_command)==1:
+            args=""
+        else:
+            args = separated_command[1]
+
+        #print("separated command=", separated_command)
+        handler = self.command_list[separated_command[0]]
+        #print(commandline)
+        return handler(self,args)
+
 
 if __name__=='__main__':
-        d = Directions("EAST")
-        #d = Directions()
-        d.turn_left()
-        print("d=", d)
-        r = Robot()
-        print("robot currently", r.direction)
-        r.turn_left()
-        print("turned to", r.direction)
-        a = CommandObjectFactory()
-        c= a.command_handler("PLACE 2,3,W")
-        c.execute(r)
+    d = Directions("EAST")
+    #d = Directions()
+    #d.turn_left()
+    d = d.whats_left()
+    print("d=", d)
+    t = Table(5,4)
+    r = Robot(t)
+    print("robot currently", r.direction)
+    r.turn_left()
+    print("turned to", r.direction)
+    a = CommandObjectFactory()
+    c= a.command_handler("PLACE 2,3,WEST")
+    c.execute(r)
+    c= a.command_handler("REPORT")
+    c.execute(r)
+    c = a.command_handler("MOVE")
+    c.execute(r)
+    c= a.command_handler("REPORT")
+    c.execute(r)
+    c = a.command_handler("RIGHT")
+    c.execute(r)
+    c= a.command_handler("REPORT")
+    c.execute(r)
+    c = a.command_handler("MOVE")
+    c.execute(r)
+    c= a.command_handler("REPORT")
+    c.execute(r)
+    c = a.command_handler("MOVE")
+    c.execute(r)
+    c= a.command_handler("REPORT")
+    c.execute(r)
+    c = a.command_handler("MOVE")
+    c.execute(r)
+    c= a.command_handler("REPORT")
+    c.execute(r)
+    c = a.command_handler("MOVE")
+    c.execute(r)
+    c= a.command_handler("REPORT")
+    c.execute(r)
+    c = a.command_handler("MOVE")
+    c.execute(r)
+    c= a.command_handler("REPORT")
+    c.execute(r)
+
+
+    print(r)
