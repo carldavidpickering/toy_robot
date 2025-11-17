8661f454fea545637dd1e9dafc14799c872488b2
Carl Pickering <carl.pickering@omatic.com>
2025-11-17 09:12:18 +0000

    We have a commandline here

---
+++ src/command_line_interface.py
@@ -0,0 +1,35 @@
+from toy_robot import Robot, CommandObjectFactory, RobotCommandMakers
+from table import Table
+from sys import stdin, stdout, stderr
+
+class CommandLineInterface:
+    def __init__(self, input_file=stdin, output_file=stdout, error_file=stderr):
+        self.input_file = input_file
+        self.output_file = output_file
+        self.error_file = error_file
+        
+        self.table = Table(5,5)
+        self.robot = Robot(self.table)
+        self.command_factory = CommandObjectFactory(RobotCommandMakers().command_list)
+
+    def process_command(self):
+        command_line = self.input_file.readline()
+        print("Receved ", command_line)
+        command_line = command_line.rstrip()
+        if command_line == "EXIT":
+            return False
+        try:
+           command_object = self.command_factory.command_parser(command_line)
+           command_object.execute(self.robot)
+        except KeyError as e:
+            self.error_file.write("ERROR: Command not recognised:"+command_line)
+        except Exception as e:
+            self.error_file.write("ERROR: Other error:"+e.__str__())
+        return True
+
+
+if __name__=="__main__":
+    processing_commands = True
+    cli = CommandLineInterface() 
+    while cli.process_command():
+        print("DONE")
--- src/toy_robot.py
+++ src/toy_robot.py
@@ -3,15 +3,21 @@ from abc import ABC, abstractmethod
 from command_factory import CommandObjectFactory
 from compass_directions import CompassDirections
 from table import Table
+from sys import stdout, stdin, stderr
 
 class Robot:
     '''This stores and operates on all the details of the robot position and status'''
-    def __init__(self, table):
+    def __init__(self, table, input_file=stdin, output_file=stdout, error_file=stderr):
         self.table = table
         self.placed = False
         self.x = 0
         self.y = 0
         self.direction = CompassDirections(CompassDirections.NORTH)
+        self.output_file = stdout
+        self.error_file = stderr
+
+    def set_output_file(self, output_file):
+        self.output_file = output_file
 
     def set_direction(self, direction):
         self.direction = direction
@@ -52,6 +58,9 @@ class Robot:
         else:
             raise ValueError("Initial position "+str(x)+","+str(y)+"Out of range.")
 
+    def report(self):
+        if self.placed:
+           self.output_file.writelines("REPORT "+str(self.x)+","+str(self.y)+","+ str(self.direction)+"\n")
 
 
 class RobotCommand(ABC):
@@ -79,7 +88,8 @@ class RobotPlaceCommand(RobotCommand):
 
 class RobotReportCommand(RobotCommand):
     def execute(self, robot):
-        print("REPORT ", robot.x, robot.y, robot.direction)
+        robot.report()
+        #print("REPORT ", robot.x, robot.y, robot.direction)
 
 class RobotMoveCommand(RobotCommand):
     def execute(self, robot):
